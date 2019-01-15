cc6a642877cb9081e7497f6633487ed451e932e0
joaquimbarreto <joaquim@barreto.co>
2019-01-15 18:14:22 +0000

    works ok

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const BOOKS_API = 'http://localhost:3000/books';
-const USERS_API = 'http://localhost:3000/users';
+const BOOKS_API = 'http://localhost:3000/books/';
+const USERS_API = 'http://localhost:3000/users/';
 const booksListEl = document.querySelector('#list');
 const showPanelEl = document.querySelector('#show-panel');
 const likesReadersEl = document.querySelector('#likes-readers');
 const readersListEl = document.querySelector('#readers');
-
 
 
 function listBooks() {
@@ -15,27 +14,49 @@ function listBooks() {
       const bookEl = document.createElement('li');
       bookEl.innerHTML = book.title;
       booksListEl.appendChild(bookEl);
-      bookEl.addEventListener('click', () => {
-        const imageEl = document.createElement('img');
-        imageEl.src = book.img_url;
-        showPanelEl.innerHTML = '';
-        readersListEl.innerHTML = '';
-        showPanelEl.innerHTML = `<h2>${book.title}</h2><p>Description: ${book.description}.</p>`
-        showPanelEl.appendChild(imageEl);
-        const readButton = document.createElement('button');
-        readButton.innerText = 'Like Book';
-        likesReadersEl.appendChild(readButton);
-        readButton.addEventListener('click', () => {
-          let bookUsers = Array.from(book.users)
-          for (const value of bookUsers) {
-            const readerEl = document.createElement('li');
-            readerEl.innerText = value.username;
-            readersListEl.appendChild(readerEl);
-          }
-        })
-      })
+      bookEl.addEventListener('click', () => getBook(book))
     });
-  });
+  })
 }
+
+function getBook(book) {
+  const imageEl = document.createElement('img');
+  const readButton = document.createElement('button');
+  imageEl.src = book.img_url;
+  showPanelEl.innerHTML = '';
+  readersListEl.innerHTML = '';
+  showPanelEl.innerHTML = `<h2>${book.title}</h2><p>Description: ${book.description}.</p>`
+  showPanelEl.appendChild(imageEl);
+  readButton.innerText = 'Like book';
+  showPanelEl.appendChild(readButton);
+  readButton.addEventListener('click', () => getReadersWhoLike(book))
+};
+
+
+function getReadersWhoLike(book){
+    const user = {"id":1, "username":"pouros"};
+    if (book.users.filter(u => u.id === user.id).length > 0) {
+      alert('Already like the book');
+    } else {
+      book.users.push(user)
+      fetch(BOOKS_API + book.id, {
+        method: 'PATCH',
+        headers: {
+          "Content-Type":"application/json"
+        },
+        body: JSON.stringify(book)
+      }).then(response => response.json())
+      .then(data => {
+        const readers = data.users;
+        for (const value of readers){
+          const readerEl = document.createElement('li');
+          showPanelEl.appendChild(readersListEl);
+          readersListEl.appendChild(readerEl);
+          readerEl.innerText = value.username;
+        }
+      })
+    }
+}
+
 
 document.addEventListener("DOMContentLoaded", listBooks);
